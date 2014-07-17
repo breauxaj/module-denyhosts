fc96ab8f11f32437432902bf97823672ab08af2c
Adam Breaux <breauxaj@gmail.com>
2014-07-17 16:38:25 -0400

    added docs

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,3 +1,31 @@
+# Define: denyhosts::service
+#
+# This define manages the Denyhosts service
+#
+# Parameters:
+#
+#  ensure:
+#    Controls the state of the service
+#
+#  enable:
+#    Controls the service start on boot
+#
+# Actions:
+#   - Stops/starts the Denyhosts service
+#
+# Requires:
+#
+#  EPEL repository
+#
+# Sample Usage:
+#
+#  To enable the service, use:
+#
+#    denyhosts::service { 'default':
+#      ensure => running,
+#      enable => true
+#    }
+#
 define denyhosts::service (
   $ensure = running,
   $enable = true
